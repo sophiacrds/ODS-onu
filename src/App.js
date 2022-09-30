510a893f32ba099cd6f551672e7b082209cc29d5
Sophia Leão <sophialeaocardoso@hotmail.com>
2022-09-30 19:56:42 -0400

    feat: create global style

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Card from "./components/Card";
+import { GlobalStyle } from "./global/GlobalStyle";
 
 export default function App() {
 	return (
 		<>
+			<GlobalStyle />
 			<Card />
 		</>
 	);
